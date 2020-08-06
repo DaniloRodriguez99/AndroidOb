a678b01caab57adb384931b48449c134ce0aa00c
DaniloRodriguez <dr52838@gmail.com>
2020-08-06 19:52:55 -0300

    Diseño Login Terminado, no tiene ninguna funcionalidad hecha aun solo diseño

--- app/src/main/java/com/example/obligatorio/MainActivity.java
+++ app/src/main/java/com/example/obligatorio/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.obligatorio;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+
+        import com.example.obligatorio.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
