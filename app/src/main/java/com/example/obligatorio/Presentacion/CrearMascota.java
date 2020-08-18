9cb874b57e8cb74527584e228078d576f13f6e75
DaniloRodriguez <dr52838@gmail.com>
2020-08-18 17:07:20 -0300

    Animacion en la pantalla de Crear Mascota

--- app/src/main/java/com/example/obligatorio/Presentacion/CrearMascota.java
+++ app/src/main/java/com/example/obligatorio/Presentacion/CrearMascota.java
@@ -2,15 +2,25 @@ package com.example.obligatorio.Presentacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
 
 import com.example.obligatorio.R;
 
 public class CrearMascota extends AppCompatActivity {
 
+    private ImageView imagenMascota;
+    private AnimationDrawable animacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_mascota);
+
+        imagenMascota = (ImageView) findViewById(R.id.imgMascota_CrearMascota);
+        imagenMascota.setBackgroundResource(R.drawable.gato_quieto_animacion);
+        animacion = (AnimationDrawable) imagenMascota.getBackground();
+        animacion.start();
     }
 }
