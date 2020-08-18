25da78f943cb8afbfbce4c298fe316b442ca03f6
DaniloRodriguez <dr52838@gmail.com>
2020-08-18 18:39:04 -0300

    mejore el metodo para insertar la animacion, porque el anterior ocupaba demasiado espacio y hacia que la app sea mucho mas lenta

--- app/src/main/java/com/example/obligatorio/Presentacion/CrearMascota.java
+++ app/src/main/java/com/example/obligatorio/Presentacion/CrearMascota.java
@@ -7,20 +7,20 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.obligatorio.R;
 
 public class CrearMascota extends AppCompatActivity {
 
-    private ImageView imagenMascota;
-    private AnimationDrawable animacion;
+    private ImageView mascota;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_mascota);
 
-        imagenMascota = (ImageView) findViewById(R.id.imgMascota_CrearMascota);
-        imagenMascota.setBackgroundResource(R.drawable.gato_quieto_animacion);
-        animacion = (AnimationDrawable) imagenMascota.getBackground();
-        animacion.start();
+        mascota = (ImageView) findViewById(R.id.imgMascota_CrearMascota);
+        String gifUrl = "file:///android_res/drawable/gato_quieto.gif";
+        Glide.with(this).load(R.drawable.gato_quieto).into(mascota);
     }
 }
